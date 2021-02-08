94c75d203835939df147f60f8f8a0a4c8f3f512c
SobkoSofiya <sobkosofiya23@gmail.com>
2021-02-08 12:38:40 +0300

    Initial Commit

---
+++ FixPwise/FixPwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixPwiseApp.swift
+//  FixPwise
+//
+//  Created by Sofi on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FixPwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
